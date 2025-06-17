3614ac5d38feeadc80bf8288b0644ab02118111d
ItsDaMango <wright.e.will@gmail.com>
2025-06-17 18:50:58 +1000

    Unreal 5.6 C++ Init
    Download of blank unreal template with c++ opened with rider to initiate default source editor

---
+++ UnrealProject/Forge_Project/Source/Forge_Project/Forge_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Forge_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Forge_Project, "Forge_Project" );
